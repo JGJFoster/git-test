730f4a7b7d78c66f30108045a88c062b0f2d3f56
John Foster <jgjfoster@outlook.com>
2020-02-12 11:25:12 -0700

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Mini Project 1 - Hello World!
+# Name
+#personalized greeting of some sort
+
+# Inputs
+name = input("Please your name: ")
+
+# The script
+print("Hello ", name, "! ... Welcome to programming!", sep="")
