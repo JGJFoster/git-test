0755c03d70ffa9c59528712c2d20816a5f9937e9
John Foster <jgjfoster@outlook.com>
2020-02-12 14:35:10 -0700

    deleted helloworld.py

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-# Mini Project 1 - Hello World!
-# Name
-#personalized greeting of some sort
-
-# Inputs
-name = input("Please your name: ")
-
-# The script
-print("Hello ", name, "! ... Welcome to programming!!", sep="")
